fdf9d17ad6b3ef2760f8fe55a83e94233ea20dc4
Mounika <mounika.aeleshwaram@poulimainfo.tech>
2024-11-05 13:18:18 +0530

    js 2

--- Questions/FizzBuzz.js
+++ Questions/FizzBuzz.js
@@ -1,3 +1,11 @@
+// Question 1: FizzBuzz
+// Task: Write a function fizzBuzz(n) that takes a number n and returns an array of strings. For numbers from 1 to n:
+
+// Return "Fizz" for multiples of 3.
+// Return "Buzz" for multiples of 5.
+// Return "FizzBuzz" for multiples of both 3 and 5.
+// Return the number itself for all other numbers.
+
 function fizzBuzz(n) {
     for (var i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
--- Questions/ReverseString.js
+++ Questions/ReverseString.js
@@ -2,12 +2,13 @@
 // Task: Implement a function reverseString(str) that takes a string as input and returns it reversed. For example, reverseString("hello") should return "olleh".
 
 
-function reverseString(str) {
-    var reversedStr = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        reversedStr += str[i];
-    }
-    return reversedStr;
-}
+var reverseString = function(str) {
+    let reversed = '';  // Initialize an empty string to store the reversed string
 
-console.log(reverseString("hello"));  // "olleh"
+    // Loop through the original string from the end to the beginning
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];  // Concatenate each character in reverse order
+    }
+
+    return reversed;  // Return the reversed string
+};
